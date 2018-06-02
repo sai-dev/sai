8912f88e42b1a34c1fc63a9ad6f3c51272798f34
Francesco Morandin <francesco.morandin@unipr.it>
2018-06-02 18:52:33 +0200

    Prepare parameters for sai10b "AlphaZero".

--- training/tf/config.py
+++ training/tf/config.py
@@ -7,11 +7,20 @@ BOARD_SQUARES = BOARD_SIZE * BOARD_SIZE
 # Learning rate
 LEARN_RATE = 0.05
 
+# Sane values are from 4096 to 64 or so. The maximum depends on the amount
+# of RAM in your GPU and the network size. You need to adjust the learning rate
+# if you change this.
+BATCH_SIZE = 512
+
+# Use a random sample of 1/16th of the input data read. This helps
+# improve the spread of games in the shuffle buffer.
+DOWN_SAMPLE = 16
+
 # Outputs new network after the specified number of training steps
-TRAINING_STEPS = 2000
+TRAINING_STEPS = 500 #2000
 
 # Display intermediate output after the specified number of training steps
-TRAINING_STEPS_VERBOSE = 500
+TRAINING_STEPS_VERBOSE = 100
 
 # Maximum number of training steps (0 continue forever)
-MAX_TRAINING_STEPS = 16000
+MAX_TRAINING_STEPS = 500 #16000
