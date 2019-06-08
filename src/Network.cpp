leela-zero/leela-zero

Allow configuring default komi at compile-time.
Pull request #2257.

src/Network.cpp
@@ -99,7 +99,7 @@ float Network::benchmark_time(int centiseconds) {
     std::atomic<int> runcount{0};
 
     GameState state;
-    state.init_game(BOARD_SIZE, 7.5);
+    state.init_game(BOARD_SIZE, KOMI);
 
     // As a sanity run, try one run with self check.
     // Isn't enough to guarantee correctness but better than nothing,

sai-dev/sai

src/Network.cpp
@@ -100,7 +100,7 @@ float Network::benchmark_time(int centiseconds) {
     std::atomic<int> runcount{0};
 
     GameState state;
-    state.init_game(BOARD_SIZE, 7.5);
+    state.init_game(BOARD_SIZE, cfg_komi);
 
     // As a sanity run, try one run with self check.
     // Isn't enough to guarantee correctness but better than nothing,