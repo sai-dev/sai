214a68f54e07f484086faa8ba15a6cfe91da6821
Gianluca Amato <gianluca.amato.74@gmail.com>
2019-10-27 15:34:35 +0100

    Improvements to VS2019 build system.

---
+++ msvc/sai9x9.bat
@@ -0,0 +1,2 @@
+@echo off
+sai9x9.exe -w 94619dea457de054503cec030269ce842c47055ba51e96db8fee841dfbaf05f9.gz --noponder -p 0 -v 1000 --komi 7.5 --lambda 0.5 -r 5 -t 6 --batchsize 5 --nrsymm --restrict_tt %1 %2 %3 %4 %5 %6 %7 %8 %9
