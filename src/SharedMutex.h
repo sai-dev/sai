4108420c5f690f6f7404b18dd1825fd196dedca4
Gianluca Amato <gianluca.amato.74@gmail.com>
2021-02-08 17:01:38 +0100

    Make LockGuard methods inline to please MSVC.

--- src/SharedMutex.h
+++ src/SharedMutex.h
@@ -82,4 +82,23 @@ private:
     SharedMutex &m_sm;
 };
 
+template<>
+inline LockGuard<lock_t::X_LOCK>::LockGuard(SharedMutex& sm) : m_sm(sm) {
+    m_sm.lock();
+}
+
+template<>
+inline LockGuard<lock_t::X_LOCK>::~LockGuard() {
+    m_sm.unlock();
+}
+
+template<>
+inline LockGuard<lock_t::S_LOCK>::LockGuard(SharedMutex& sm) : m_sm(sm) {
+    m_sm.lock_shared();
+}
+
+template<>
+inline LockGuard<lock_t::S_LOCK>::~LockGuard() {
+    m_sm.unlock_shared();
+}
 #endif
